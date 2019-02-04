be5398e3abb7f3061b317fc164c2bfcef0ee5906
jprcoder <smartcoder91@outlook.com>
2019-02-05 05:53:49 +0800

    react stacked bar chart

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BarChart from './components/BarChart';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			chartData: {},
+		};
+	}
+	componentWillMount() {
+		this.getChartData();
+	}
+
+	getChartData() {
+		// Ajax Calls here
+		this.setState({
+			chartData: {
+				labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambrige', 'New Bedford'],
+				datasets: [
+					{
+						label: 'Population1',
+						backgroundColor: [
+							'rgba(255, 99, 132, 0.6)',
+							'rgba(54, 162, 235, 0.6)',
+							'rgba(255, 206, 86, 0.6)',
+							'rgba(75, 192, 192, 0.6)',
+							'rgba(153, 102, 255, 0.6)',
+							'rgba(255, 159, 64, 0.6)',
+							'rgba(255, 99, 132, 0.6)',
+						],
+						borderColor: 'rgb(255, 99, 132)',
+						data: [617594, 181045, 153060, 106519, 105162, 105162],
+          },
+          {
+						label: 'Population2',
+						backgroundColor: [
+							'rgba(255, 154, 104, 0.6)',
+							'rgba(137, 221, 255, 0.6)',
+							'rgba(224, 255, 163, 0.6)',
+							'rgba(163, 255, 193, 0.6)',
+							'rgba(213, 163, 255, 0.6)',
+							'rgba(255, 219, 163, 0.6)',
+							'rgba(215, 99, 132, 0.6)',
+						],
+						borderColor: 'rgb(255, 99, 132)',
+						data: [517594, 171045, 154106, 96519, 95162, 95162],
+					},  
+				],
+			},
+		}, () => {
+      console.log('chartData', this.state.chartData);
+    });
+		
+	}
+	render() {
+		return (
+			<div className="App">
+				<BarChart chartData={this.state.chartData} location="New York" legendPostion="bottom" />
+        
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/BarChart.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+import { Bar, Line, Pie } from 'react-chartjs-2';
+
+class BarChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            chartData: props.chartData
+        }
+    }
+	static defaultProps = {
+		displayTitle: true,
+		displayLegend: true,
+        legendPostion: 'right',
+        location: 'City'
+	};
+	render() {
+        console.log(this.state.chartData)
+		return (
+			<div>
+				<Bar
+					data={this.state.chartData}
+					options={{
+                        scales: {
+                            xAxes: [{ stacked: true }],
+                            yAxes: [{ stacked: true }]
+                        },
+						title: {
+							display: this.props.displayTitle,
+							text: 'Largest Cities In ' + this.props.location,
+							fontSize: 25,
+						},
+						legend: {
+							display: this.props.displayLegend,
+							position: this.props.legendPostion,
+						},
+					}}
+				/>
+                <Line
+					data={this.state.chartData}
+					options={{
+                        scales: {
+                            xAxes: [{ stacked: true }],
+                            yAxes: [{ stacked: true }]
+                        },
+						title: {
+							display: this.props.displayTitle,
+							text: 'Largest Cities In ' + this.props.location,
+							fontSize: 25,
+						},
+						legend: {
+							display: this.props.displayLegend,
+							position: this.props.legendPostion,
+						},
+					}}
+				/>
+                <Pie
+					data={this.state.chartData}
+					options={{
+                        scales: {
+                            xAxes: [{ stacked: true }],
+                            yAxes: [{ stacked: true }]
+                        },
+						title: {
+							display: this.props.displayTitle,
+							text: 'Largest Cities In ' + this.props.location,
+							fontSize: 25,
+						},
+						legend: {
+							display: this.props.displayLegend,
+							position: this.props.legendPostion,
+						},
+					}}
+				/>
+			</div>
+		);
+	}
+}
+
+export default BarChart;
